9835f38a0852e5355d6105e187f8a381529dea91
Piotr Kaminski <pkaminski@piotrkaminski.pl>
2015-12-22 07:28:57 +0100

    Revert main ino file, somehow content was lost.

--- SiweckiSmartHome.ino
+++ SiweckiSmartHome.ino
@@ -1,11 +1,44 @@
-//The setup function is called once at startup of the sketch
-void setup()
-{
-// Add your initialization code here
-}
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
 
-// The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+#include "Actor.h"
+#include "Room.h"
+#include "LightPoint.h"
+
+const String ACTOR_NAME = "Ard1";
+const String LIVINGROOM_NAME = "LivingRoom";
+const String CORRIDOR_NAME = "Corridor";
+const int CORRIDOR_BUTTON_PIN = 2;
+const int CORRIDOR_OUTPUT_PIN = 3;
+const String CEILING_NAME = "Ceiling";
+const int CEILING_BUTTON_PIN = 6;
+const int CEILING_OUTPUT_PIN = 7;
+
+byte mac[6] = {0x00, 0x12, 0xFB, 0x95, 0x59, 0xCF};
+
+byte mqttServerIP[4] = {192, 168, 1, 190};
+int mqttServerPort = 1883;
+
+const int AFTER_CHANGE_DELAY = 100;
+
+Actor actor = Actor(ACTOR_NAME);
+ControllerConnector controllerConnector = ControllerConnector();
+
+EthernetClient ethClient;
+PubSubClient mqttClient = PubSubClient(mqttServerIP, mqttServerPort, callback, ethClient);
+
+LightPoint *ceiling = NULL;
+LightPoint *corridor = NULL;
+int i = 0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  char* command = (char*)payload;
+  Serial.print("Queue: ");
+  Serial.println(topic);
+  Serial.print("Message received: ");
+  Serial.println(command);
+  actor.executeCommand(topic, command);
+  Serial.println("Callback finished.");
 }
